ca11ef7aee8a1dc2a319a05a8e68f167600f3f44
Ashour <ashour.omar@siemens.com>
2024-11-23 10:08:23 +0100

    Tailwind added with initial map

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import { BrowserRouter } from "react-router";
 import { Chart } from 'react-charts'
 import { GridStack } from 'gridstack';
@@ -11,31 +11,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const position = [51.505, -0.09]
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div>
 
+      <MapContainer center={position} height={160} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
 
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <p className='text-3xl font-bold'>OmarAshour</p>
+
+    </div>
+
   )
 }
 
