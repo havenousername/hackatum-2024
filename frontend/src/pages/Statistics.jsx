7090536f0ac4a92a5980b51ccbf6254205ebddab
Ashour <ashour.omar@siemens.com>
2024-11-23 20:50:57 +0100

    other charts added

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -1,8 +1,9 @@
 
 import { GridStack } from 'gridstack';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import BarChart from '../charts/bar';
-
+import { faker } from '@faker-js/faker';
+import TimeChart from '../charts/TimeChart';
 const Statistics = () => {
 
     const [statistics, _] = useState(
@@ -14,7 +15,77 @@ const Statistics = () => {
 
         ]
     );
+    //fakedata for timechart
+    const [car] = useState({
+        id: 'randomId',
+        isAvailable: true,
+        customer: 'customerId',
+        shortSummary: 'a car had more than 400 rides with 200 people',
+        mostImportantMetricks: {
+            availability: '90%',
+            energySpend: '30%',
+        },
+        metrics: {
+            distance: {
+                withCustomer: '80%',
+                without: '20%'
+            },
+            numberOfTrips: 499,
+            errors: 2,
+            averageTravelTime: '6min',
+            energyFootprint: '4% from norm'
+        }
+    });
 
+    const [dataset] = useState({
+        hourly: {
+            data: [10, 15, 20, 35],
+            timeLabels: ["10:30", "10:40", "10:50", "11:00"],
+        },
+        daily: {
+            data: [190, 105, 200, 350],
+            timeLabels: ["10:30", "10:40", "10:50", "11:00"],
+        },
+    });
+
+    const options = useMemo(() => Object.keys(dataset).map(key => ({ 
+        value: dataset[key],
+        label: `${key[0].toUpperCase()}${key.slice(1)}`
+    })), [dataset]);
+
+    const [selectedData, setSelectedData] = useState(options[0]);
+
+    ///
+
+    //fakedata for barchart
+    const labels = ['1st simulation', '2nd simulation', '3rd simulation'];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Avg. Fleet Efficiency',
+                data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+                backgroundColor: 'rgba(231, 164, 150, 1)',
+            },
+            {
+                label: 'Avg. Operational',
+                data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+                backgroundColor: 'rgba(225, 100, 73, 1)',
+            },
+            {
+                label: 'Avg. Env. Impact',
+                data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+                backgroundColor: 'rgba(246, 236, 234, 1)',
+            },
+
+            {
+                label: 'Avg. General',
+                data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+                backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            },
+        ],
+    };
+    ///
     useEffect(() => {
         GridStack.init();
     }, []);
@@ -35,7 +106,7 @@ const Statistics = () => {
                 <div className="rounded-xl bg-dark-charcoal ">
                     <div className="p-8 border-dark">
                         <div className='rounded-xl p-4 bg-charcoal-gray'>
-                            <div className=""><BarChart /></div>
+                            <div className=""><BarChart data={data} /></div>
                             <div className='pl-8'>
                                 <h4 className='pt-4'>Statistics</h4>
                                 <div className='grid pt-2 grid-cols-2'>
@@ -49,10 +120,10 @@ const Statistics = () => {
                     </div>
                     <div className="p-8 gap-2 grid-cols-2 grid border-dark">
                         <div className='rounded-xl p-8 bg-charcoal-gray'>
-                            <div className=""><BarChart /></div>
+                            <div className=""><TimeChart {...selectedData.value} /></div>
                         </div>
                         <div className='rounded-xl p-8 bg-charcoal-gray'>
-                            <div className=""><BarChart /></div>
+                            <div className=""><TimeChart {...selectedData.value} /></div>
                         </div>
                     </div>
                 </div>
