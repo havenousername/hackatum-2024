a1e55b62cbc5af33ee55fe9ddb4f6d387d4c2a56
Ashour <ashour.omar@siemens.com>
2024-11-23 13:41:08 +0100

    small change in the tab margin and roundness

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,12 +5,12 @@ const NavBar = () => {
     const [isMapView, setIsMapView] = useState(true);
     
     return (
-        <div className="w-[39rem] h-16 rounded-3xl bg-charcoal-gray absolute z-[999] left-1/2 mt-10 transform -translate-x-1/2">
+        <div className="w-[39rem] h-16 rounded-full bg-charcoal-gray absolute z-[999] left-1/2 mt-10 transform -translate-x-1/2">
             <div className="flex justify-around items-center h-full">
                 <div className="w-6/12 cursor-pointer">
                     <div onClick={() =>{
                         setIsMapView(true)
-                    }}  className={`text-center rounded-3xl w-11/12 ml-2 flex justify-center items-center h-10 ${isMapView ? 'bg-burnt-coral' : ''}`}>
+                    }}  className={`text-center rounded-3xl w-11/12 ml-4 flex justify-center items-center h-10 ${isMapView ? 'bg-burnt-coral' : ''}`}>
                         Map View
                     </div>
                 </div>
@@ -18,7 +18,7 @@ const NavBar = () => {
                 <div className="w-6/12 cursor-pointer">
                     <div onClick={() =>{
                         setIsMapView(false)
-                    }} className={`text-center rounded-3xl w-11/12 ml-2 flex justify-center items-center h-10 ${!isMapView ? 'bg-burnt-coral' : ''}`}>
+                    }} className={`text-center rounded-3xl w-11/12 mr-4 flex justify-center items-center h-10 ${!isMapView ? 'bg-burnt-coral' : ''}`}>
                         Statistics
                     </div>
                 </div>
