8e773305f0f08b48f7b25480835acd41b20c4626
Ashour <ashour.omar@siemens.com>
2024-11-24 00:47:29 +0100

    logo added with interaction

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,21 +3,25 @@ import Parameters from '../components/Parameters';
 import MapComponent from '../components/Map';
 import CarTooltip from '../components/CarTooltip';
 import 'leaflet/dist/leaflet.css';
+import Logo from '../assets/logo.svg?react'
 
 
 function Home() {
-  const [parametersPosition] = useState({bottom: '5%', left: '5%'});
+  const [parametersPosition] = useState({ bottom: '5%', left: '5%' });
   const [isOpen, setIsOpen] = useState(true);
-
+  const [isExpanded, setIsExpanded] = useState(false);
   const onMapClickCar = () => {
     setIsOpen(true);
   }
 
   return (
     <div className='w-full h-screen relative'>
-      <Parameters position={parametersPosition} />
+      <span onClick={() => { setIsExpanded(true) }}>
+        <Logo className={`absolute z-[99999] bottom-8 left-8 cursor-pointer ${isExpanded ? "hidden" : ""}`} width={"5rem"} height={"5rem"} />
+      </span>
+      <Parameters isExpanded={isExpanded} setIsExpanded={setIsExpanded} position={parametersPosition} />
       <CarTooltip isOpen={isOpen} setIsOpen={(isOpen) => setIsOpen(isOpen)} />
-      <MapComponent onClickCar={onMapClickCar}/>
+      <MapComponent onClickCar={onMapClickCar} />
     </div>
   )
 }
