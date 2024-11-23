a127505791578f57de8182ca4d89f3d38d6ad817
havenousername <andrei.cristea997@gmail.com>
2024-11-23 15:16:03 +0100

    fix: add new markets to the map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,11 +1,31 @@
 import { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import LeafletListeners from './LeafletListeners';
-import carWithPersonIcon from "../assets/car-light-with-person.svg"
+import carWithPersonIconSource from "../assets/car-light-with-person.svg"
+import carIconSource from "../assets/car-light.svg"
+import destinationIconSource from "../assets/destination-light.svg"
+import customerIconSource from '../assets/person-light.svg'
 import { Icon } from 'leaflet';
+import '../assets/overlay.css';
 
-const customIcon = new Icon({
-  iconUrl: carWithPersonIcon, 
+const customerIcon = new Icon({
+  iconUrl: customerIconSource, 
+  iconSize: [32, 32]
+});
+
+const carIcon = new Icon({
+  iconUrl: carIconSource,
+  iconSize: [32, 32]
+})
+
+const carWithPersonIcon = new Icon({
+  iconUrl: carWithPersonIconSource, 
+  iconSize: [32, 32]
+});
+
+
+const customerDestinationIcon = new Icon({
+  iconUrl: destinationIconSource, 
   iconSize: [32, 32]
 });
 
@@ -25,18 +45,26 @@ function generateRandomPoint([lat, lon], radius) {
   return [randomLat, randomLon];
 }
 
+const ONE_KM = 1000;
+
 const MapComponent = () => {
   const [center] = useState([51.505, -0.09]);
-  const [carPositions] = useState(Array.from({ length: 10 }).fill([]).map(_ => generateRandomPoint(center, 1000)));
+  const [carPositions] = useState(Array.from({ length: 10 }).fill([]).map(_ => generateRandomPoint(center, ONE_KM)));
+  const [customerPositions] = useState(Array.from({ length: 10 }).fill([]).map(_ => generateRandomPoint(center, ONE_KM)));
+  const [customerWithCarsPositions] = useState(Array.from({ length: 10 }).fill([]).map(_ => generateRandomPoint(center, ONE_KM)));
+  const [customerDestinationPositions] = useState(Array.from({ length: 10 }).fill([]).map(_ => generateRandomPoint(center, ONE_KM)));
 
   return (
     <MapContainer center={center} zoom={13} className='h-screen'>
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
+        url="https://cdn.lima-labs.com/{z}/{x}/{y}.png?api=demo"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
       />
       <LeafletListeners />
-      { carPositions.map((position, key) => <Marker key={key} position={position} icon={customIcon} />) }
+      { carPositions.map((position, key) => <Marker key={key} position={position} icon={carIcon} />) }
+      { customerPositions.map((position, key) => <Marker key={key} position={position} icon={customerIcon} />)}
+      { customerWithCarsPositions.map((position, key) => <Marker key={key} position={position} icon={carWithPersonIcon} />)}
+      { customerDestinationPositions.map((position, key) => <Marker key={key} position={position} icon={customerDestinationIcon} />)}
     </MapContainer>
   );
 };
--- frontend/src/components/Parameters.jsx
+++ frontend/src/components/Parameters.jsx
@@ -3,25 +3,37 @@ import Slider from "./Slider.jsx";
 import shrinkIcon from "../assets/shrink.svg"
 
 const Parameters = ({ position }) => {
-    const [tradeoffOne, setTradeoffOne] = useState(0);
-    const [tradeoffTwo, setTradeoffTwo] = useState(20);
+    const [time, setTime] = useState(50);
+    const [distance, setDistance] = useState(50);
+    const [revenue, setRevenue] = useState(50);
+    const [energy, setEnergy] = useState(50);
     
     return (
         <div style={{  ...position }} className="bg-[#2E2E2E] rouded-xl absolute px-9 py-6 rounded-lg max-w-[550px] w-full z-[999]">
             <img src={shrinkIcon} alt="shrink icon" className="absolute right-6 top-6" />
-            <h4 className="font-bold text-2xl">Parameters</h4>
+            <h4 className="font-bold text-2xl">Importance Parameters</h4>
             <div className="py-2">
-                <Slider value={tradeoffOne} onChange={(value) => setTradeoffOne(value)} />
+                <Slider value={time} onChange={(value) => setTime(value)} />
                 <div className="flex justify-between">
-                    <h6>Customer Friendly</h6>
-                    <h6>Error-less</h6>
+                    <h6>Time</h6>
                 </div>
             </div>
             <div className="py-2">
-                <Slider value={tradeoffTwo} onChange={(value) => setTradeoffTwo(value)} />
+                <Slider value={distance} onChange={(value) => setDistance(value)} />
                 <div className="flex justify-between">
-                    <h6>Fleet Efficiency</h6>
-                    <h6>Eco Friendliness</h6>
+                    <h6>Distance</h6>
+                </div>
+            </div>
+            <div className="py-2">
+                <Slider value={revenue} onChange={(value) => setRevenue(value)} />
+                <div className="flex justify-between">
+                    <h6>Time</h6>
+                </div>
+            </div>
+            <div className="py-2">
+                <Slider value={energy} onChange={(value) => setRevenue(value)} />
+                <div className="flex justify-between">
+                    <h6>Renenue</h6>
                 </div>
             </div>
         </div>
