47f9d4e9ef698886c91c23a6e73911402069283d
Ashour <ashour.omar@siemens.com>
2024-11-23 12:43:37 +0100

    integrate colors in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,16 +7,14 @@ export default {
   theme: {
     extend: {
       zIndex: {
-        '999':'999'
-      },
-      colors:{
-        'charcoal-gray' : '#2E2E2E',
-        'burnt-coral' : '#E16449'
+        '999': '999'
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
       colors: {
+        'charcoal-gray': '#2E2E2E',
+        'burnt-coral': '#E16449',
         primary: {
           '400': '#EFBBB0',
           '800': '#E16449'
