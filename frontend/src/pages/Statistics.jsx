122fb9d81e4f659e40e6a696925cbc1361616cd0
Ashour <ashour.omar@siemens.com>
2024-11-23 21:24:06 +0100

    Second column of the dashboard done without readjustments

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -4,16 +4,26 @@ import { useEffect, useState, useMemo } from 'react';
 import BarChart from '../charts/bar';
 import { faker } from '@faker-js/faker';
 import TimeChart from '../charts/TimeChart';
+import PieChart from '../charts/PieChart';
 const Statistics = () => {
 
     const [statistics, _] = useState(
-        [
-            { name: 'Avg. Fleet Efficiency', percentage: 56 },
-            { name: 'Avg. Operational', percentage: 99.99 },
-            { name: 'Avg. Env. Impact', percentage: 56 },
-            { name: 'Avg. General', percentage: 80.9 },
-
-        ]
+        {
+            statistics: [
+                { name: 'Avg. Fleet Efficiency', value: 56 },
+                { name: 'Avg. Operational', value: 99.99 },
+                { name: 'Avg. Env. Impact', value: 56 },
+                { name: 'Avg. General', value: 80.9 }
+            ],
+            customer: [
+                { name: 'Avg. Waiting Time', value: 5 },
+                { name: 'Avg. Spent Time', value: 12 }
+            ],
+            fleet: [
+                { name: 'Avg. Customer Time', value: 4 },
+                { name: 'Avg. Spent Time', value: 12 }
+            ]
+        }
     );
     //fakedata for timechart
     const [car] = useState({
@@ -48,7 +58,7 @@ const Statistics = () => {
         },
     });
 
-    const options = useMemo(() => Object.keys(dataset).map(key => ({ 
+    const options = useMemo(() => Object.keys(dataset).map(key => ({
         value: dataset[key],
         label: `${key[0].toUpperCase()}${key.slice(1)}`
     })), [dataset]);
@@ -110,8 +120,8 @@ const Statistics = () => {
                             <div className='pl-8'>
                                 <h4 className='pt-4'>Statistics</h4>
                                 <div className='grid pt-2 grid-cols-2'>
-                                    {statistics.map((data, index) =>
-                                        <div key={index} className='flex gap-6'> <h6>{data.name}</h6> <h6>{`${data.percentage}%`}</h6></div>
+                                    {statistics.statistics.map((data, index) =>
+                                        <div key={index} className='flex gap-6'> <h6>{data.name}</h6> <h6>{`${data.value}%`}</h6></div>
                                     )
                                     }
                                 </div>
@@ -128,14 +138,37 @@ const Statistics = () => {
                     </div>
                 </div>
 
-                <div className="rounded-xl bg-dark-charcoal ">
-                    <div className="border-dark">
-                        <div className="">Item 1</div>
+                <div className='grid grid-rows-2 gap-20 rounded-xl p-8 bg-dark-charcoal'>
+                    <div className="grid grid-cols-2 gap-x-2 rounded-xl gap-4 p-8 bg-charcoal-gray ">
+
+                        <div className="border-dark">
+                            <div className="pt-8"><PieChart /></div>
+                            <div>
+                                <h4 className='pt-14'>Customer Statistics</h4>
+                                {statistics.customer.map((data, index) =>
+                                    <div key={index} className='flex gap-6'> <h6>{data.name}</h6> <h6>{`${data.value} min`}</h6></div>)
+                                }
+
+                            </div>
+                        </div>
+                        <div className="border-dark">
+                            <div className="pt-8"><PieChart /></div>
+                            <h4 className='pt-14'>Fleet Statistics</h4>
+                                {statistics.fleet.map((data, index) =>
+                                    <div key={index} className='flex gap-6'> <h6>{data.name}</h6> <h6>{`${data.value} min`}</h6></div>)
+                                }
+                        </div>
                     </div>
-                    <div className="border-dark">
-                        <div className="">Item 2</div>
+                    <div className="rounded-xl p-8 bg-charcoal-gray ">
+
+                        <div className="border-dark">
+                            <div className=""><PieChart /></div>
+                        </div>
                     </div>
+
                 </div>
+
+
 
                 <div className="rounded-xl bg-dark-charcoal ">
                     <div className="border-dark">
