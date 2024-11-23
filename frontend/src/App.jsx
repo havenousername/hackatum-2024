48096c41d833f15b4885f0050d1b2c3277c0c83f
Ashour <ashour.omar@siemens.com>
2024-11-23 10:49:32 +0100

    initial map view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import { BrowserRouter } from "react-router";
 import { Chart } from 'react-charts'
 import { GridStack } from 'gridstack';
+import { useEffect } from 'react';
+import MapComponent from './map';
+import 'leaflet/dist/leaflet.css';
 
-import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,23 +17,10 @@ function App() {
   const position = [51.505, -0.09]
 
   return (
-    <div>
-
-      <MapContainer center={position} height={160} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-
+    <>
+      <MapComponent />
       <p className='text-3xl font-bold'>OmarAshour</p>
-
-    </div>
+    </>
 
   )
 }
