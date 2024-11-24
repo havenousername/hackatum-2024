771da2c080c9f9ee99ebc039f456190c671c3414
Jakob Eigenmann <ga27hib@mytum.de>
2024-11-24 01:40:59 +0100

    Updated main function and fixed a bug in the simulation

--- backend/main.py
+++ backend/main.py
@@ -1,51 +1,43 @@
+from simulation import Simulation
 from greedy import greedy_vehicle_assignment
 from mip import mip_complete_route_assignment
 from backend_api import create_scenario, initialize_scenario, launch_scenario, get_scenario
 from monitoring import *
-from simulation import Simulation
+from websocket import main
 
 
 NUMBER_OF_VEHICLES = 2
 NUMBER_OF_CUSTOMERS = 5
 
 
-if __name__ == '__main__':
-    # Step 1: Create a scenario with 2 vehicles and 2 customers
+def create_scenario_with_api_call(num_vehicles, num_customers):
     try:
-        scenario = create_scenario(number_of_vehicles=NUMBER_OF_VEHICLES, number_of_customers=NUMBER_OF_CUSTOMERS)
+        scenario = create_scenario(number_of_vehicles=num_vehicles, number_of_customers=num_customers)
         print(f"Scenario created with ID: {scenario.id}")
+        ret_id = scenario.id
+        initialize_scenario(payload=scenario, db_scenario_id=str(scenario.id))
     except Exception as e:
         print(f"Error creating scenario: {e}")
         exit(1)
+    return ret_id
 
-    # Step 2: Initialize the scenario using the scenario ID
+def calculate_mapping(scenario_id):
     try:
-        response = initialize_scenario(payload=scenario, db_scenario_id=str(scenario.id))
-        print("Scenario initialized.")
-    except Exception as e:
-        print(f"Error initializing scenario: {e}")
-        exit(1)
-
-    # Step 3: Launch the scenario with a specific speed
-    try:
-        launch_response = launch_scenario(scenario_id=str(scenario.id), speed=0.2)
-        print("Scenario launched.")
-    except Exception as e:
-        print(f"Error launching scenario: {e}")
-        exit(1)
-
-    # Step 4: Use the greedy algorithm to assign vehicles to customers
-
-    try:
-        mapping = mip_complete_route_assignment(scenario_id=scenario.id)
+        mapping = mip_complete_route_assignment(scenario_id=scenario_id)
     except Exception as e:
         print(f"Error running the threads: {e}")
         exit(1)
-    scenario = get_scenario(scenario.id)
+    scenario = get_scenario(scenario_id)
     customers = scenario.customers
     vehicles = scenario.vehicles
+    return vehicles, customers, mapping
+
+
+if __name__ == '__main__':
+    scenario_id = create_scenario_with_api_call(NUMBER_OF_VEHICLES, NUMBER_OF_CUSTOMERS)
+    vehicles, customers, mapping = calculate_mapping(scenario_id)
 
     simulation = Simulation(vehicles, customers, mapping)
-    print(simulation.state(10000))
+    print(simulation.state(4000))
     for taxi in vehicles:
         print(simulation.taxi_finish_time(taxi))
